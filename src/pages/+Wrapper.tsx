664e5324391a081fdf42d2ddcca9d454786389aa
NathanielJamie <nathanzambranauchani@gmail.com>
2025-12-10 15:01:00 +0000

    eden=intelligent

--- src/pages/+Wrapper.tsx
+++ src/pages/+Wrapper.tsx
@@ -1,6 +1,7 @@
 import { useGoogleFonts } from '../lib/use-google-fonts';
 
 import '../index.css';
+import { Fragment } from 'react/jsx-runtime';
 
 export default function Wrapper({ children }: { children: React.ReactNode }) {
 	useGoogleFonts([['Source Code Pro', '100,200,300,400,500,600,700,800,900']], {
@@ -8,5 +9,8 @@ export default function Wrapper({ children }: { children: React.ReactNode }) {
 		display: 'swap',
 	});
 
-	return children;
+	return <Fragment>
+		<header className='flex justify-center items-center border-b border-black/50 h-17'>I AM A HEADER</header>
+		{children}
+	</Fragment>;
 }
--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -1,10 +1,23 @@
+import axios from 'axios';
+import { ReactNode, useEffect, useState } from 'react';
 import { Fragment } from 'react/jsx-runtime';
 
 export default function Page() {
+	const [repoCards, setRepoCards] = useState<Array<ReactNode>>()
+
+	useEffect(() => {
+		axios.get('https://api.github.com/users/NathanielJamie/repos').then((d) => {
+			const data = d.data as Array<{name: string}>;
+
+			setRepoCards(data.map((d, index) => (
+				<p key={index} className='flex items-center p-6 m-0 bg-blue-800 w-fit rounded-lg'>{d.name}</p>
+			)))
+		})
+	}, []);
+
 	return (
-		<Fragment>
-			
-			<p className='px-50'>I feel so sigma!</p>
-		</Fragment>
+		<div className='flex flex-col gap-3'>
+			{repoCards}
+		</div>
 	);
 }

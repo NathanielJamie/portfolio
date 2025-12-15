a6e396426f1a54c936f1f9144c0aed665d1b00eb
NathanialJamie <nathanzambranauchani@gmail.com>
2025-12-15 12:14:57 +0000

    nuh uh

--- src/pages/index/+Page.tsx
+++ src/pages/index/+Page.tsx
@@ -3,21 +3,21 @@ import { ReactNode, useEffect, useState } from 'react';
 import { Fragment } from 'react/jsx-runtime';
 
 export default function Page() {
-	const [repoCards, setRepoCards] = useState<Array<ReactNode>>()
+	const [repoCards, setRepoCards] = useState<Array<ReactNode>>();
 
 	useEffect(() => {
 		axios.get('https://api.github.com/users/NathanielJamie/repos').then((d) => {
-			const data = d.data as Array<{name: string}>;
+			const data = d.data as Array<{ name: string }>;
 
-			setRepoCards(data.map((d, index) => (
-				<p key={index} className='flex items-center p-6 m-0 bg-blue-200 w-fit rounded-lg'>{d.name}</p>
-			)))
-		})
+			setRepoCards(
+				data.map((d, index) => (
+					<p key={index} className='flex items-center p-6 m-0 bg-red-800 w-fit rounded-lg'>
+						{d.name}
+					</p>
+				))
+			);
+		});
 	}, []);
 
-	return (
-		<div className='flex flex-col gap-3'>
-			{repoCards}
-		</div>
-	);
+	return <div className='flex flex-col gap-3'>{repoCards}</div>;
 }

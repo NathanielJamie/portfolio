034856968112ed90c961f248b6d34e8e77ea7f1b
NathanialJamie <nathanzambranauchani@gmail.com>
2025-12-15 18:50:34 +0000

    everything now

--- src/pages/+Wrapper.tsx
+++ src/pages/+Wrapper.tsx
@@ -1,18 +1,26 @@
-import { useGoogleFonts } from '../lib/use-google-fonts';
-
-import '../index.css';
 import { Fragment } from 'react/jsx-runtime';
+import { useGoogleFonts } from '../lib/use-google-fonts';
+import '../index.css';
+import Portfolio from './portfolio'; // import your Portfolio component with correct +page filename
 
-export default function Wrapper({ children }: { children: React.ReactNode }) {
-	useGoogleFonts([['Source Code Pro', '100,200,300,400,500,600,700,800,900']], {
-		addBodyClass: true,
-		display: 'swap',
-	});
+export default function Wrapper() {
+  useGoogleFonts(
+    [['Source Code Pro', '100,200,300,400,500,600,700,800,900']],
+    {
+      addBodyClass: true,
+      display: 'swap',
+    }
+  );
 
-	return (
-		<Fragment>
-			<header className='flex justify-center items-center border-b border-black/50 h-17'>THIS IS GERRAD @</header>
-			{children}
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      {/* Header */}
+      <header className="flex justify-center items-center h-20 text-4xl font-bold text-white bg-gray-900 drop-shadow-lg">
+        THIS IS GERRAD @
+      </header>
+
+      {/* Portfolio Content */}
+      <Portfolio />
+    </Fragment>
+  );
 }
